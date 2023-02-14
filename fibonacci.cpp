1ce2a8f452901c5d3c41a4397337ddab4081fe9e
Jesus-Almansa <jtafernandez@gmail.com>
2023-02-14 16:55:56 +0100

    Code

---
+++ fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+// number of Fibonacci terms to generate
+void Fibonacci(int n){
+    double t1 = 0, t2 = 1, nextTerm;
+
+    std::cout << "Fibonacci Series:" << std::endl;
+
+    for (int i = 1; i <= n; ++i) {
+        std::cout << t1 << " ";
+
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+}
+
+
+int main() {
+
+    Fibonacci(10);
+
+    return 0;
+}
